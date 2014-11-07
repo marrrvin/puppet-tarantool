0dc7c12edaa921ae4b27aa71d2e3a4416e3f39d5
Sergey Orlov <foobar@list.ru>
2014-11-07 23:26:18 +0000

    Manifest fixes.

--- manifests/client.pp
+++ manifests/client.pp
@@ -1,5 +1,5 @@
 
-class tarantool::client($package_name='tarantool-client', $version='latest') {
+class tarantool::client($package_name='tarantool-client', $package_ersion='latest') {
     package { $package_name:
       ensure => $package_version
     }
--- manifests/init.pp
+++ manifests/init.pp
@@ -1,5 +1,8 @@
 
 class tarantool {
+    Class['tarantool::server'] -> Class['tarantool::service']
+
     class { 'tarantool::server': }
     class { 'tarantool::client': }
+    class { 'tarantool::service': }
 }
