0957d10c8188acbe6b540d6e1afa9c4bfd5f3982
Sergey Orlov <foobar@list.ru>
2014-11-08 09:35:56 +0000

    Fix install class.

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,5 +1,5 @@
 
-class tarantool::server($package_name='tarantool', $package_version='latest') {
+class tarantool::install($package_name='tarantool', $package_version='latest') {
   package { $package_name:
     ensure => $package_version,
   }
